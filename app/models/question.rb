ae60da37d37847e6427b9d76693e244f203b09c1
keylimotino <stephanie48758@gmail.com>
2018-06-14 18:14:35 +0000

    automatically backed up by learn

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,45 @@
+##  What's your favorite color?
+##  options = ["red", "blue", "yellow"]
+
+## if the user chooses "red" --> then user gets 1 point --> add to a total score
+## if the user chooses "blue"--> user gets 2 points --> add to a total score
+## if the user chooses "yellow" --> user gets 3 points --> add to a total score
+
+
+
+
 class Question
+@@question = []
+
+  def initialize(number, question)
+    @number = number
+    @question = question
+  end
+
+  def number= (number)
+    @number = number
+  end
+
+  def number
+    @number
+  end
+
+  def question= (question)
+    @question = question
+  end
+
+  def question
+    @question
+  end
 
 end
+
+question = {
+  :id => "Q1",
+  :text => "Are you tired"
+  }
+puts question.text
+
+
+
+##  Use the .each do method to iterate through questions.
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,34 @@
+Total_score = 0
+class Question
+  def initialize(id, text, answers)
+  end
+end
+
+question_1 = Question.new
+question_1.id = "Q1"
+question_1.text = "What's your favorite color?"
+question_1.answers = {
+  "blue" => 1,
+  "green" => 2,
+  "yellow" => 3
+}
+
+
+
+
 class Result
 
+
+
 end
+
+
+cruel_result = Result.new(
+  :id => "R1",
+  :score => "1-8 points"
+  :text => "You are cruel."
+  )
+
+stuck_up = Result.new(
+  :id => "R2",
+)
